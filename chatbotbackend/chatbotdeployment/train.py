3254c4d2bb1d97b67c21dbf1ed382e64e1122e82
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2023-04-16 21:35:23 +0530

    some changes to model

--- chatbotbackend/chatbotdeployment/train.py
+++ chatbotbackend/chatbotdeployment/train.py
@@ -1,15 +1,16 @@
 import numpy as np
 import random
 import json
-
+import os
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
+current_dir = os.path.dirname(__file__)
+jsonfile_path = os.path.join(current_dir, 'intents.json')
 
-with open('intents.json', 'r') as f:
+with open(jsonfile_path, 'r') as f:
     intents = json.load(f)
 
 all_words = []
